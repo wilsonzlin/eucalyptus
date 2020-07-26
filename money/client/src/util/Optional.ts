f2c43f685238d0de8263263b06a221afb843bb5f
Wilson Lin <code@wilsonl.in>
2020-07-26 15:09:58 +1000

    Move some client helpers to extlib

--- money/client/src/util/Optional.ts
+++
@@ -1,3 +0,0 @@
-export const mapDefined = <V, R> (val: V | undefined, mapper: (val: V) => R): R | undefined => val === undefined ? undefined : mapper(val);
-
-export const exists = <V> (val: V | null | undefined): val is V => val != undefined;
